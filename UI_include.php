b096c43864a22b8d042298f0de95da0e7dde8e80
Raff Evald <rafael-ev-silva@outlook.com>
2022-06-22 14:56:13 -0300

    Refatorando projeto

---
+++ UI_include.php
@@ -0,0 +1,4 @@
+<?php
+define("INC_DIR", $_SERVER["DOCUMENT_ROOT"] . "/patinhas-cars-dev-web/login/includes/");
+define("ASSET_DIR", $_SERVER["DOCUMENT_ROOT"] . "/patinhas-cars-dev-web/login/assets/");
+define("FUNC_DIR", $_SERVER["DOCUMENT_ROOT"] . "/patinhas-cars-dev-web/login/functions/");
