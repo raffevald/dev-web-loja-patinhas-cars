dfe7e9e87cbb1f43ce3af8f055205424957c78fb
Raff Evald <rafael-ev-silva@outlook.com>
2022-06-23 14:52:29 -0300

    refatorando codigo

--- UI_include.php
+++ UI_include.php
@@ -1,4 +1,5 @@
 <?php
-define("INC_DIR", $_SERVER["DOCUMENT_ROOT"] . "/patinhas-cars-dev-web/includes/");
-define("ASSET_DIR", $_SERVER["DOCUMENT_ROOT"] . "/patinhas-cars-dev-web/assets/");
-define("FUNC_DIR", $_SERVER["DOCUMENT_ROOT"] . "/patinhas-cars-dev-web/functions/");
+    define("INC_DIR", $_SERVER["DOCUMENT_ROOT"] . "/patinhas-cars-dev-web/includes/");
+    define("ASSET_DIR", $_SERVER["DOCUMENT_ROOT"] . "/patinhas-cars-dev-web/assets/");
+    define("FUNC_DIR", $_SERVER["DOCUMENT_ROOT"] . "/patinhas-cars-dev-web/functions/");
+?>
